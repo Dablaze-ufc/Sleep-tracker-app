ddae981783b5ed47284438b4823bb5689502632c
dablaze-ufc <chukwuwauchenna@gmail.com>
2020-05-03 11:01:54 +0100

    added recyclerView, implemented viewHolder and adapter and a util method to convert the sleep data to string

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -3,10 +3,12 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
@@ -21,6 +23,16 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
        val singleNight = data[position]
         val res = holder.itemView.context.resources
         holder.sleepQuality.text = convertDurationToFormatted(singleNight.startTimeMilli, singleNight.endTimeMilli,res)
+        holder.quality.text = convertNumericQualityToString(singleNight.sleepQuality,res)
+        holder.imageQuality.setImageResource(when (singleNight.sleepQuality){
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
 
     }
 
@@ -30,6 +42,7 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val sleepQuality:TextView = itemView.findViewById(R.id.sleep_length)
         val quality:TextView = itemView.findViewById(R.id.quality_string)
+        val imageQuality :ImageView = itemView.findViewById(R.id.quality_image)
     }
 
 
